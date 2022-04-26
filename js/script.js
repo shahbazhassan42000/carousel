71d9db2812d93f95970a952f49a25e02465cb65b
Shahbazhassan42000 <Shahbazhassan42000@gmail.com>
2022-04-26 05:49:43 +0500

    all setting add up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let images = document.querySelectorAll(".image");
+    images = [...images]
+    const controls = document.getElementById("controls");
 
+    controls.addEventListener("click", function (event) {
+        event.preventDefault();
+        let btn = event.target;
+        if (btn.tagName === "BUTTON") {
+            removeClasses();
+            if (btn.id === "prevBtn") {
+                images.unshift(images.pop());
+            } else {
+                images.push(images.shift());
+            }
+            addClasses();
+        }
+    });
+
+    function removeClasses() {
+        for (let img of images) {
+            for (let i = 1; i <= 5; i++) {
+                let className = "img-" + i.toString();
+                img.classList.remove(className);
+                img.style.backgroundSize="100px";
+            }
+        }
+    }
+
+    function addClasses() {
+        for (let i = 1; i <= 5; i++) {
+            let className = "img-" + i.toString();
+            images[i].classList.add(className);
+            if(i===1 || i===5){
+                images[i].style.backgroundSize="100px";
+            }
+            else if(i===2 || i===4){
+                images[i].style.backgroundSize="200px";
+            }
+            else{
+                images[i].style.backgroundSize="300px";
+            }
+        }
+    }
 });
